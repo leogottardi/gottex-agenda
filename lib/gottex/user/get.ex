17bd57ae57974255dfe38800391d2b61166d866a
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-06 12:32:49 -0300

    feat: add get user by email

--- lib/gottex/user/get.ex
+++ lib/gottex/user/get.ex
@@ -7,16 +7,24 @@ defmodule Gottex.User.Get do
   end
 
   defp handle_get(%{"id" => id}), do: validate_uuid(id)
+  defp handle_get(%{"email" => email}), do: find_user({:ok, :email, email})
 
   defp validate_uuid(id) do
     case Ecto.UUID.cast(id) do
-      {:ok, uuid} -> find_user({:ok, uuid})
+      {:ok, uuid} -> find_user({:ok, :id, uuid})
       :error -> {:error, "ID não é um UUID"}
     end
   end
 
-  defp find_user({:ok, uuid}) do
+  defp find_user({:ok, :id, uuid}) do
     case Repo.get(User, uuid) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+
+  defp find_user({:ok, :email, email}) do
+    case Repo.get_by(User, %{email: email}) do
       nil -> {:error, "User not found"}
       user -> {:ok, user}
     end
