120aff417eb8fe37fdc7c224a68cb2cf94c96727
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-08 19:54:09 -0300

    fix: removendo log

--- lib/gottex/contact/create.ex
+++ lib/gottex/contact/create.ex
@@ -2,9 +2,6 @@ defmodule Gottex.Contact.Create do
   alias Gottex.{Contact, Repo}
 
   def call(params) do
-    IO.inspect(params)
-
-    
     params
     |> Contact.changeset()
     |> Repo.insert()
