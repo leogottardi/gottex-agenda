3e98fb1e620f85fd07fa4ebc8e5ca931786e4064
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-09 06:58:15 -0300

    feat: excluindo struct de contact

--- lib/gottex_web/controllers/contacts_controller.ex
+++ lib/gottex_web/controllers/contacts_controller.ex
@@ -1,14 +1,12 @@
 defmodule GottexWeb.ContactsController do
   use GottexWeb, :controller
 
-  alias Gottex.Contact.Struct, as: ContactStruct
-
   action_fallback GottexWeb.FallbackController
 
   def create(conn, params) do
     with {:ok, user, _claims} <- GottexWeb.Auth.Guardian.get_resource_from_token(conn),
-         %ContactStruct{} = contact_struct <- ContactStruct.build(params, user.id),
-         {:ok, contact} <- Gottex.create_contact(Map.from_struct(contact_struct))
+         contact_params <- Map.put(params, "user_id", user.id),
+         {:ok, contact} <- Gottex.create_contact(contact_params)
       do
 
       conn
