6cd3ad98b39b5f092ef06ff97656b43aea6471b5
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-12 19:56:39 -0300

    remove use unused

--- lib/gottex_web/auth/guardian.ex
+++ lib/gottex_web/auth/guardian.ex
@@ -1,8 +1,6 @@
 defmodule GottexWeb.Auth.Guardian do
   use Guardian, otp_app: :gottex
 
-  import Plug.Conn
-
   def subject_for_token(%{id: id}, _claims) do
     sub = to_string(id)
     {:ok, sub}
