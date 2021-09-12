c24ccbec18287d50d44e0c30858c6e16c99d0b4c
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-12 11:29:30 -0300

    feat: adicionando teste de create e get de usuario

--- test/gottex/user_test.exs
+++ test/gottex/user_test.exs
@@ -1,5 +1,6 @@
 defmodule Gottex.UserTest do
   use ExUnit.Case
+  use Gottex.DataCase
 
   alias Gottex.User
 
@@ -20,5 +21,32 @@ defmodule Gottex.UserTest do
                           } = user
     end
 
+    test "create/1, will a valid params return a valid user" do
+      params = %{name: "Leonardo", email: "leomgottardi@gmail.com", password: "123456"}
+
+      user = User.Create.call(params)
+
+      assert {:ok, %Gottex.User{
+                                id: _id,
+                                email: "leomgottardi@gmail.com",
+                                name: _name,
+                                password_hash: _password_hash
+                              }} = user
+    end
+
+    test "get/1, will a valid params return a valid user" do
+      params = %{name: "Leonardo", email: "leomgottardi@gmail.com", password: "123456"}
+
+      {:ok, create_user} = User.Create.call(params)
+
+      user = User.Get.call(%{"email" => create_user.email})
+
+      assert {:ok, %Gottex.User{
+                          email: "leomgottardi@gmail.com",
+                          name: "Leonardo",
+                          password_hash: _password_hash,
+                          recovery_code: nil
+                        }} = user
+    end
   end
 end
